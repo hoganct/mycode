0a0b510104969c1c8239af192162c6a3fce32a8e
hoganct <hoganct.300@gmail.com>
2023-05-24 22:19:47 +0000

    playing with python pip packages

---
+++ if_name/first_module.py
@@ -0,0 +1,12 @@
+print(f"This code will always execute.")
+
+def main():
+    print(f"This code belongs to the function in module 1.")
+
+if __name__ == "__main__":
+    main()
+
+"""if __name__ == "__main__":
+    print(f"Code is being run directly from Python.")
+else:
+    print(f"Code is being run indirectly from import.")"""
