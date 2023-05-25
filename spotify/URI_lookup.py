46e3817d4e26654e10092cefe6ffb758877277fa
hoganct <hoganct.300@gmail.com>
2023-05-25 10:55:13 -0700

    URI lookup updated

--- spotify/URI_lookup.py
+++ spotify/URI_lookup.py
@@ -1,24 +1,27 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-# Spotify API credentials
+# storing my Spotify API credentials
 CLIENT_ID = '349309ae5ef349eb85096837bc86547b'
 CLIENT_SECRET = 'c06e23caa93b489db429fc3ac6233ad6'
 
-# Authenticate with Spotify
-client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+def lookup():
+    # Authenticate with Spotify
+    client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-# Prompt the user for an artist name
-artist_name = input("Enter an artist name: ")
+    # Prompt the user for an artist name
+    artist_name = input("Enter an artist name: ")
 
-# Search for the artist using Spotify API
-results = spotify.search(q='artist:' + artist_name, type='artist', limit=1)
+    # Search for the artist using Spotify API
+    results = spotify.search(q='artist:' + artist_name, type='artist', limit=1)
 
-# Get the artist URI from the search results
-if results['artists']['items']:
-    artist = results['artists']['items'][0]
-    artist_uri = artist['uri']
-    print("Artist URI:", artist_uri)
-else:
-    print("Artist not found.")
+    # Get the artist URI from the search results
+    if results['artists']['items']:
+        artist = results['artists']['items'][0]
+        artist_uri = artist['uri']
+        print("Artist URI:", artist_uri)
+    else:
+        print("Artist not found.")
+
+lookup()
