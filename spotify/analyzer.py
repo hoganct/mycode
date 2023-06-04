da2d23b799c46d725b097738248966b4d52deaa4
hoganct <hoganct.300@gmail.com>
2023-06-04 13:45:20 -0700

    formatting and bokeh improvements

--- spotify/analyzer.py
+++ spotify/analyzer.py
@@ -1,28 +1,93 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+<<<<<<< HEAD
 
 # reading csv made from artist_explorer.py
 df = pd.read_csv('50_tracks.csv', encoding='latin1') # using 'latin1' encoding due to some weird errors for some songs, only thing that seems to consistently work
+=======
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import HoverTool, CategoricalColorMapper
+from bokeh.layouts import column
+from bokeh.models.widgets import Div
 
-# select the columns from the 8th field onwards - just the floats and tempo, no strings
-data_columns = df.columns[7:]
+# Reading CSV made from artist_explorer.py
+df = pd.read_csv('50_tracks.csv', encoding='utf-8')
+>>>>>>> cddf393 (formatting and bokeh improvements)
 
-# normalizing with Min-Max scaling - this will take tempo and convert it into a 0-1 value
+# Select the columns from the 8th field onwards - just the floats and tempo, no strings
+data_columns = df.columns[7:-1]  # Exclude the last column "Embed Code"
+
+# Normalizing with Min-Max scaling - this will take tempo and convert it into a 0-1 value
 scaler = MinMaxScaler()
 df[data_columns] = scaler.fit_transform(df[data_columns])
 
-# calculate the variances for each field
+# Calculate the variances for each field
 variances = df[data_columns].var()
 
-# sort the fields based on their variances in descending order
+# Sort the fields based on their variances in descending order
 sorted_variances = variances.sort_values(ascending=False)
 print("All of the variances:")
 print(sorted_variances)
 
-# select the top two fields with the highest variances
+# Select the top two fields with the highest variances
 top_two_fields = sorted_variances.head(2)
 
+<<<<<<< HEAD
 # print the names of the top two fields
 print("Top two fields with the most variance:")
 for field_name, variance in top_two_fields.items():
     print(field_name)
+=======
+print(f"The two fields with the greatest variance are {field1} and {field2}.\n")
+
+# Ask the user if they want to use the fields with the greatest variance or define their own fields
+user_choice = input("Would you like select your own categories to analyze? (Y/N): ")
+
+if user_choice.lower() == 'y':
+    field1 = input("Enter Field 1: ")
+    field2 = input("Enter Field 2: ")
+
+# Print the names of the selected fields
+print("\nThe two selected fields are:")
+print(field1)
+print(field2)
+
+# Get unique artists
+artists = df['Artist Name'].unique()
+
+# Define a distinct color palette with a vibrant orange
+color_palette = ['#FF0000', '#FF8000', '#000080', '#008000', '#00FFFF']
+
+# Create a color mapper mapping each artist to a color
+num_artists = len(artists)
+palette = color_palette[:num_artists]
+color_mapper = CategoricalColorMapper(factors=artists, palette=palette)
+
+# Create the scatterplot with colored points
+scatterplot = figure()
+scatterplot.circle(x=field1, y=field2, source=df, fill_color={'field': 'Artist Name', 'transform': color_mapper}, size=6, legend_field='Artist Name')
+
+# Customize hover tooltips
+hover = HoverTool(tooltips=[("Artist", "@{Artist Name}"), ("Track", "@{Track Name}")])
+scatterplot.add_tools(hover)
+
+# Set plot attributes
+scatterplot.xaxis.axis_label = field1
+scatterplot.yaxis.axis_label = field2
+
+# Get the HTML data from the last field for each record
+html_data = df.iloc[:, -1]
+
+# Create a Div element to display the HTML data
+html_content = Div(text='', width=800, height=200)
+
+# Concatenate all the HTML data and update the Div element
+html_content.text = '\n\n'.join(html_data)
+
+# Create a layout with the scatterplot and the Div element
+layout = column(scatterplot, html_content)
+
+# Save the plot as an HTML file and show it
+output_file("scatterplot.html")
+show(layout)
+>>>>>>> cddf393 (formatting and bokeh improvements)
--- spotify/artist_explorer.py
+++ spotify/artist_explorer.py
@@ -1,56 +1,80 @@
 import csv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import requests
 
+<<<<<<< HEAD
 # storing my Spotify API credentials - delete before commit!
 CLIENT_ID = 
 CLIENT_SECRET = 
+=======
+# Storing my Spotify API credentials - delete before commit!
+CLIENT_ID = '349309ae5ef349eb85096837bc86547b'
+CLIENT_SECRET = '2c5d4880a00a4bba80c915b55ccb54b5'
+>>>>>>> cddf393 (formatting and bokeh improvements)
 
-# authenticate with Spotify
+# Authenticate with Spotify
 client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-# defining CSV and field names
+# Defining CSV and field names
 csv_file = '50_tracks.csv'
 field_names = ['Artist Name', 'Artist URI', 'Track Name', 'Track URI', 'Album',
                'Key', 'Mode', 'Danceability', 'Energy', 'Loudness', 'Speechiness',
-               'Acousticness', 'Instrumentalness', 'Liveness', 'Valence', 'Tempo']
+               'Acousticness', 'Instrumentalness', 'Liveness', 'Valence', 'Tempo', 'Embed Code']
 
-with open(csv_file, 'w', newline='') as file: # opening our csv to append
-        writer = csv.DictWriter(file, fieldnames=field_names) # using writer to write to csv
-        writer.writeheader() # writing the header
+with open(csv_file, 'w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=field_names)
+    writer.writeheader()
+
 
 def artist_lookup():
-    # prompt the user for an artist name
+    # Prompt the user for an artist name
     user_artist = input("Enter an artist: ")
 
-    # search for the artist using Spotify API
+    # Search for the artist using Spotify API
     results = sp.search(q='artist:' + user_artist, type='artist', limit=1)
 
-    # get the artist URI from the search results
+    # Get the artist URI from the search results
     if results['artists']['items']:
         artist = results['artists']['items'][0]
-        artist_uri = artist['uri'] # last two lines will save the 0th/first item from results as the artist_uri
-        top_tracks(artist_uri, user_artist) # pass artist_uri, user_artist as arguments to top_tracks function
-        related_artists(artist_uri) # pass artist_uri to related_artists function
+        artist_uri = artist['uri']
+        top_tracks(artist_uri, user_artist)
+        related_artists(artist_uri)
     else:
-        print("Artist not found.") # if results doesn't return artists, items, else
+        print("Artist not found.")
+
 
 def top_tracks(artist_uri, user_artist):
-    results = sp.artist_top_tracks(artist_uri) # takes the artist_uri as an argument for top tracks
-    tracks = results['tracks'] # pulls just tracks from the results
+    results = sp.artist_top_tracks(artist_uri)
+    tracks = results['tracks']
 
     print("\n\nThe top 10 tracks for the artist are...\n")
+<<<<<<< HEAD
     with open(csv_file, 'a', newline='') as file: # opening our csv to append
         writer = csv.DictWriter(file, fieldnames=field_names) # using writer to write to csv
+=======
+    with open(csv_file, 'a', newline='', encoding="utf-8") as file:
+        writer = csv.DictWriter(file, fieldnames=field_names)
+>>>>>>> cddf393 (formatting and bokeh improvements)
         for track in tracks:
-            track_uri = track['uri'][14:] # pulls just the uri, drops the first 14 characters (spotify:track:)
-            track_info = sp.track(track_uri) # gets additional track info for each track from track_uri
-            audio_features = sp.audio_features(track_uri) # gets audio features, defined as  below, for each track
+            track_uri = track['uri'][14:]
+            track_info = sp.track(track_uri)
+            audio_features = sp.audio_features(track_uri)
+
+            # Perform Spotify oEmbed lookup
+            oembed_api_url = (f'https://open.spotify.com/oembed?url=https://open.spotify.com/track/{track_uri}&format=json')
+            response = requests.get(oembed_api_url)
+
+            if response.status_code == 200:
+                oembed_data = response.json()
+                embedded_html = oembed_data['html']
+            else:
+                embedded_html = ''
 
             writer.writerow({
                 'Artist Name': user_artist,
-                'Artist URI': artist_uri[15:], # dropping 15 characters here
+                'Artist URI': artist_uri[15:],
                 'Track Name': track['name'],
                 'Track URI': track_uri,
                 'Album': track_info['album']['name'],
@@ -64,20 +88,22 @@ def top_tracks(artist_uri, user_artist):
                 'Instrumentalness': audio_features[0]['instrumentalness'],
                 'Liveness': audio_features[0]['liveness'],
                 'Valence': audio_features[0]['valence'],
-                'Tempo': audio_features[0]['tempo']
-            }) # might want to modify some values here, but this is good for now
+                'Tempo': audio_features[0]['tempo'],
+                'Embed Code': embedded_html
+            })
 
             print(f"{track['name']}")
 
-def related_artists(artist_uri): # creating a function to find related artists
+
+def related_artists(artist_uri):
     print("\nAnd 4 related artists... ")
-    related = sp.artist_related_artists(artist_uri) # passing artist_uri into sp.artist_related_artists
+    related = sp.artist_related_artists(artist_uri)
     related_artists_uris = []
     for artist in related['artists'][:4]:
         print(artist['name'])
         artist_uri = artist['uri']
-        related_artists_uris.append(artist_uri) # appending to the list
-        top_tracks(artist_uri, artist['name']) # sending this through top_tracks
-        #print(related_artists_uris)
+        related_artists_uris.append(artist_uri)
+        top_tracks(artist_uri, artist['name'])
+
 
 artist_lookup()
